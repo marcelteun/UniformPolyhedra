46fe15b4906757916223a30e415b5e2352b849e2
Marcel Tunnissen <marcelteun@gmail.com>
2010-10-31 00:46:42 +0200

    New mode MW89
    Signed-off-by: Marcel Tunnissen <marcelteun@gmail.com>

---
+++ MW89.bas.10.A5xI.D5C5.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+import Geom3D
+import GeomTypes
+import math
+
+u = 1.0
+tau = (1 + math.sqrt(5))/2
+tau2 = tau + 1
+_tau = tau - 1
+_tau2 = 2 - tau
+
+t = tau * u
+d = _tau * u
+s = tau2 * u
+
+Vs = [
+	GeomTypes.Vec3([ u, -t,  s])/2,
+	GeomTypes.Vec3([ s, -u,  t])/2,
+	GeomTypes.Vec3([ t,  0,  0]),
+	GeomTypes.Vec3([ s,  u, -t])/2,
+	GeomTypes.Vec3([ u,  t, -s])/2,
+	GeomTypes.Vec3([-u,  t, -s])/2,
+	GeomTypes.Vec3([-s,  u, -t])/2,
+	GeomTypes.Vec3([-t,  0,  0]),
+	GeomTypes.Vec3([-s, -u,  t])/2,
+	GeomTypes.Vec3([-u, -t,  s])/2,
+]
+
+#for v in Vs:
+#    print v
+
+Fs = [
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+]
+
+C_scaled = [0, t, 1] # scaled by 5/(tau2 + 1)
+
+print "Use A5xI | D5C5"
+print "where A5xI O3 = [1, 1, 1]"
+print "where A5xI O5 _|_ O3 =", C_scaled
+print "----------------------------"
+print "where D5xI O5 =", C_scaled
+print "where D5xI H2 _|_ O5 = [1, 0, 0]"
+
+shape = Geom3D.SimpleShape(Vs = Vs, Fs = Fs)
---
+++ MW89.bas.3.A5xI.D3C3.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import Geom3D
+import GeomTypes
+import math
+
+u = 1.0
+tau = (1 + math.sqrt(5))/2
+tau2 = tau + 1
+_tau = tau - 1
+_tau2 = 2 - tau
+
+t = tau * u
+d = _tau * u
+s = tau2 * u
+
+Vs = [
+        GeomTypes.Vec3([ 0, 0, t]),
+        GeomTypes.Vec3([ u, t, s])/2,
+        GeomTypes.Vec3([-u, t, s])/2,
+]
+
+Fs = [
+        [0, 1, 2],
+]
+
+O5 = [0, t, 1]
+
+print "Use A5xI | D3C3"
+print "where A5xI O3 = [1, 1, 1]"
+print "where A5xI O5 _|_ O3 =", O5
+print "----------------------------"
+print "where D3C3 O3 = ", [0, d, t]
+print "where D3C3 H2 _|_ O3 = [1, 0, 0]"
+
+shape = Geom3D.SimpleShape(Vs = Vs, Fs = Fs)
