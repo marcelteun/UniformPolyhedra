e19f0c77d0d1224b1e4f3dc85be202023db20165
Marcel Tunnissen <marcelteun@gmail.com>
2010-11-09 22:16:09 +0100

    Add MW75
    Signed-off-by: Marcel Tunnissen <marcelteun@gmail.com>

---
+++ MW75.bas.5_3.A5xI.D5C5.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+import Geom3D
+import GeomTypes
+import math
+
+u = 1
+tau = (1 + math.sqrt(5))/2
+_tau = tau - 1
+_tau2 = 2 - tau
+_tau3 = _tau * _tau2
+tau2 = tau + 1
+dtau = 2*tau
+tau3 = dtau + 1
+
+t = tau * u
+s = tau2 * u
+T = tau3 * u
+d = _tau * u
+w = _tau2 * u
+D = _tau3 * u
+
+Pentagon = [
+	GeomTypes.Vec3([ D,	u,	u]),
+	GeomTypes.Vec3([ w,	2*d,	d]),
+	GeomTypes.Vec3([ 0,	2*u-d,	w]),
+	GeomTypes.Vec3([-w,	2*d,	d]),
+	GeomTypes.Vec3([-D,	u,	u]),
+]
+
+l = len(Pentagon)
+
+pentagon = []
+for i in range(l):
+    diagonal = Geom3D.Line3D(Pentagon[i], Pentagon[(i+2)%l])
+    pentagon.append(diagonal.getPoint(_tau2))
+
+Vs = []
+for i in range(l):
+    Vs.append(Pentagon[i])
+    Vs.append(pentagon[i])
+
+#for v in Vs:
+#    print v
+
+Fs = [
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+]
+
+C_scaled = [0, t, 1] # scaled by 5/(tau2 + 1)
+
+print "Use A5xI | D5C5"
+print "where A5xI O3 = [1, 1, 1]"
+print "where A5xI O5 _|_ O3 =", C_scaled
+print "----------------------------"
+print "where D5C5 O5 =", C_scaled
+print "where D5C5 H2 _|_ O5 = [1, 0, 0]"
+
+shape = Geom3D.SimpleShape(Vs = Vs, Fs = Fs)
